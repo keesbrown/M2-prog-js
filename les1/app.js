7e9e72aed0e29ebb2f6bfa16e3b70ad9dca9d1fd
unknown <kees.brown1@gmail.com>
2022-11-21 10:53:58 +0100

    pjotr

---
+++ les1/app.js
@@ -0,0 +1,14 @@
+class App 
+{
+        runApplication()
+        {
+            console.log("hello world!")
+            // code gaat hier!
+            let pjotr = "why not?"
+            console.log(pjotr)
+        }
+}
+
+let app = new App();
+app.runApplication();
+
